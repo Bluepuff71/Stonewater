2e5cce04c7d4a04fe0dedbb47e9d678278a0f314
Emery Porter <emeryporter99@gmail.com>
2019-05-29 09:56:12 -0500

    Moved to resources

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public abstract class Interactable : MonoBehaviour
+{
+    GameObject contextPanel;
+    void OnMouseDown()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("ContextMenu"));
+        contextPanel = Resources.Load<GameObject>(@"Prefabs/Context Menu Panel");
+        Instantiate(contextPanel, GameData.ui.transform);
+    }
+}

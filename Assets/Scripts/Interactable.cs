b072516822e510297d12def85c6fd4813fd3720a
Emery Porter <emeryporter99@gmail.com>
2019-05-29 11:34:03 -0500

    Added an outline to selected objects

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Outline))]
 public abstract class Interactable : MonoBehaviour
 {
+    static GameObject currentlySelected;
     GameObject contextPanel;
+
+    void Start()
+    {
+        GetComponent<cakeslice.Outline>().eraseRenderer = true;
+    } 
     void OnMouseDown()
     {
-        Destroy(GameObject.FindGameObjectWithTag("ContextMenu"));
+        if (currentlySelected)
+        {
+            currentlySelected.GetComponent<cakeslice.Outline>().eraseRenderer = true;
+            Destroy(GameObject.FindGameObjectWithTag("ContextMenu"));
+        }
+        currentlySelected = gameObject;
         contextPanel = Resources.Load<GameObject>(@"Prefabs/Context Menu Panel");
+        contextPanel.GetComponentInChildren<Text>().text = gameObject.name;
+        GetComponent<cakeslice.Outline>().eraseRenderer = false;
         Instantiate(contextPanel, GameData.ui.transform);
     }
 }

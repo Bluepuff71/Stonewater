394ef35700c4c3a1ce5c7aea0a10453e907796fb
Emery Porter <emeryporter99@gmail.com>
2019-05-30 14:12:31 -0500

    Continued building room builder

--- Assets/Scripts/Editor/RoomBuilder.cs
+++ Assets/Scripts/Editor/RoomBuilder.cs
@@ -2,6 +2,7 @@
 using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class RoomBuilder : EditorWindow
 {
@@ -9,16 +10,40 @@ public class RoomBuilder : EditorWindow
     Vector3 cameraPos = Vector3.zero;
     Vector3 cameraRotVector = Vector3.zero;
     Quaternion cameraRotQuad;
+
+    bool isMainCamera = false;
     GameObject roomOrganizer;
     AudioClip audioClip = null;
-
+    bool continueMusicWhereLeftOff = false;
+    bool loopRoomMusic = false;
 
     List<AudioClip> roomMusic = new List<AudioClip>();
     int roomMusicSize = 0;
     float musicVolume = .5f;
 
     bool musicFoldout;
+    enum TeleporterShape
+    {
+        Box = 0,
+        Cylinder = 1,
+    }
+    TeleporterShape teleporterShape = TeleporterShape.Box;
+    Vector3 teleportPos = Vector3.zero;
+    AudioClip teleportSound;
 
+    AudioClip arriveSound;
+
+    int numOfArrivalPoints;
+
+    bool customFadeLength = false;
+
+    float customFadeOutLength = 1;
+    float customFadeInLength = 1;
+
+    bool roomHeadFoldout = false;
+    bool musicHeadFoldout = false;
+    bool cameraHeadFoldout = false;
+    bool teleportHeadFoldout = false;
 
     [MenuItem("DND World/Create Room")]
     public static void ShowWindow()
@@ -28,64 +53,133 @@ public class RoomBuilder : EditorWindow
 
     void OnGUI()
     {
-        GUILayout.Label("Room Settings", EditorStyles.boldLabel);
-        roomOrganizer = EditorGUILayout.ObjectField("Room Organizer Object", roomOrganizer, typeof(GameObject), allowSceneObjects: true) as GameObject;
-        roomName = EditorGUILayout.TextField("Room Name", roomName);
-        GUILayout.Label("Music Settings", EditorStyles.boldLabel);
-
-        musicFoldout = EditorGUILayout.Foldout(musicFoldout, "Room music");
-
-        if (musicFoldout)
+        roomHeadFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(roomHeadFoldout, "Room Settings");
+        if (roomHeadFoldout)
         {
-            EditorGUI.indentLevel = 2;
-            roomMusicSize = EditorGUILayout.DelayedIntField("Size", roomMusicSize);
-            if(roomMusicSize > 0)
-            {
-                if(roomMusicSize != roomMusic.Capacity)
-                {
-                    roomMusic = new List<AudioClip>(new AudioClip[roomMusicSize]);
-                }
-                for (int i = 0; i < roomMusicSize; i++)
-                {
-                    roomMusic[i] = EditorGUILayout.ObjectField(string.Format("Song {0}", i), roomMusic[i], typeof(AudioClip), allowSceneObjects: false) as AudioClip;
-                }
-            }
-            EditorGUILayout.BeginHorizontal();
-            if (GUILayout.Button("Add Song"))
-            {
-                roomMusicSize++;
-            }
-            if(roomMusicSize > 0)
-            {
-                if (GUILayout.Button("Remove Song"))
-                {
-                    roomMusicSize--;
-                }
-            }
-            EditorGUILayout.EndHorizontal();
+            EditorGUI.indentLevel++;
+            roomOrganizer = EditorGUILayout.ObjectField("Room Organizer", roomOrganizer, typeof(GameObject), allowSceneObjects: true) as GameObject;
+            roomName = EditorGUILayout.TextField("Room Name", roomName);
+            EditorGUI.indentLevel--;
         }
-        EditorGUI.indentLevel = 0;
-        musicVolume = EditorGUILayout.Slider("Room Music Volume", musicVolume, 0, 1);
-
-        GUILayout.Label("Camera", EditorStyles.boldLabel);
-        cameraPos = EditorGUILayout.Vector3Field("Camera Position", cameraPos);
-        cameraRotQuad = Quaternion.Euler(EditorGUILayout.Vector3Field("Camera Rotation", cameraRotVector));
-        if (GUILayout.Button("Align With Camera"))
+        EditorGUILayout.EndFoldoutHeaderGroup();
+        musicHeadFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(musicHeadFoldout, "Music Settings");
+        if (musicHeadFoldout)
         {
-            cameraPos = SceneView.lastActiveSceneView.camera.transform.position;
-            cameraRotVector = SceneView.lastActiveSceneView.camera.transform.rotation.eulerAngles;
+
+            EditorGUI.indentLevel++;
+            musicFoldout = EditorGUILayout.Foldout(musicFoldout, "Room music");
+            if (musicFoldout)
+            {
+                EditorGUI.indentLevel++;
+                roomMusicSize = EditorGUILayout.DelayedIntField("Size", roomMusicSize);
+                if (roomMusicSize > 0)
+                {
+                    if (roomMusicSize != roomMusic.Capacity)
+                    {
+                        roomMusic = new List<AudioClip>(new AudioClip[roomMusicSize]);
+                    }
+                    for (int i = 0; i < roomMusicSize; i++)
+                    {
+                        roomMusic[i] = EditorGUILayout.ObjectField(string.Format("Song {0}", i), roomMusic[i], typeof(AudioClip), allowSceneObjects: false) as AudioClip;
+                    }
+                }
+                EditorGUILayout.BeginHorizontal();
+                if (GUILayout.Button("Add Song"))
+                {
+                    roomMusicSize++;
+                }
+                if (roomMusicSize > 0)
+                {
+                    if (GUILayout.Button("Remove Song"))
+                    {
+                        roomMusicSize--;
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUI.indentLevel--;
+            musicVolume = EditorGUILayout.Slider("Room Music Volume", musicVolume, 0, 1);
+            continueMusicWhereLeftOff = EditorGUILayout.ToggleLeft("Restart music when entering room?", continueMusicWhereLeftOff);
+            loopRoomMusic = EditorGUILayout.ToggleLeft("Loop the room music?", loopRoomMusic);
+            EditorGUI.indentLevel--;
         }
-        GUILayout.Label("Teleporter", EditorStyles.boldLabel);
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        cameraHeadFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(cameraHeadFoldout, "Camera Settings");
+
+        if (cameraHeadFoldout)
+        {
+            EditorGUI.indentLevel++;
+            cameraPos = EditorGUILayout.Vector3Field("Camera Position", cameraPos);
+            cameraRotQuad = Quaternion.Euler(EditorGUILayout.Vector3Field("Camera Rotation", cameraRotVector));
+            if (GUILayout.Button("Align With Camera"))
+            {
+                cameraPos = SceneView.lastActiveSceneView.camera.transform.position;
+                cameraRotVector = SceneView.lastActiveSceneView.camera.transform.rotation.eulerAngles;
+            }
+            isMainCamera = EditorGUILayout.ToggleLeft("Should this be the main camera?", isMainCamera);
+            EditorGUI.indentLevel--;
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        teleportHeadFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(teleportHeadFoldout, "Teleporter Settings");
+
+        if (teleportHeadFoldout)
+        {
+            EditorGUI.indentLevel++;
+            teleporterShape = (TeleporterShape) EditorGUILayout.EnumPopup("Teleporter Shape", teleporterShape);
+            teleportPos = EditorGUILayout.Vector3Field("Teleporter Position", teleportPos);
+            if (GUILayout.Button("Align With Camera"))
+            {
+                teleportPos = SceneView.lastActiveSceneView.camera.transform.position;
+                //Cast a ray to the gound to get the height
+            }
+
+
+            teleportSound = EditorGUILayout.ObjectField("Teleport Sound", teleportSound, typeof(AudioClip), allowSceneObjects: false) as AudioClip;
+            arriveSound = EditorGUILayout.ObjectField("Arrive Sound", arriveSound, typeof(AudioClip), allowSceneObjects: false) as AudioClip;
+            numOfArrivalPoints = EditorGUILayout.IntSlider("# of Arrival Points", numOfArrivalPoints, 1, 6);
+
+            customFadeLength = EditorGUILayout.BeginToggleGroup("Custom Fade Lengths?", customFadeLength);
+
+            if (customFadeLength)
+            {
+                customFadeOutLength = EditorGUILayout.Slider("Fade Out Length", customFadeOutLength, .3f, 10);
+                customFadeInLength = EditorGUILayout.Slider("Fade In Length", customFadeInLength, .3f, 10);
+            }
+            EditorGUILayout.EndToggleGroup();
+        }
+
+        EditorGUILayout.EndFoldoutHeaderGroup();
         if (GUILayout.Button("Create Room"))
         {
             if (roomOrganizer)
             {
-                //GameObject room = new GameObject(roomName);
-                //room.AddComponent<Room>()
-                ////Instantiate(, roomOrganizer.transform);
-                //Camera roomCamera = new Camera();
-                ////roomCamera.
-                ////Instantiate<Camera>()
+                GameObject roomObj = new GameObject(roomName);
+                roomObj.transform.SetParent(roomOrganizer.transform);
+                Room room = roomObj.AddComponent<Room>();
+                room.roomMusic = roomMusic;
+                room.roomMusicVolume = musicVolume;
+                room.continueMusicWhereLeftOff = continueMusicWhereLeftOff;
+                room.loopRoomMusic = loopRoomMusic;
+                GameObject roomCameraObj = new GameObject(string.Format("{0} Camera", roomName));
+                roomCameraObj.transform.SetParent(roomObj.transform);
+                Camera roomCamera = roomCameraObj.AddComponent<Camera>();
+                roomCamera.transform.position = cameraPos;
+                roomCamera.transform.rotation = cameraRotQuad;
+                roomCameraObj.AddComponent<AudioListener>();
+                roomCamera.gameObject.AddComponent<cakeslice.OutlineEffect>();
+                GameObject currentMainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+                if (isMainCamera)
+                {
+                    if (currentMainCamera)
+                    {
+                        currentMainCamera.tag = "";
+                    }
+                    roomCameraObj.tag = "MainCamera";
+                }
+                GameObject teleportObj = new GameObject("Teleporter");
+
             }
             else
             {

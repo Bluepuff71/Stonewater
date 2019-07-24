304fccad8c2486c04272e549c73ec651e14bfb8f
Emery Porter <emeryporter99@gmail.com>
2019-07-24 18:49:56 +0200

    Auto assetbundle building

---
+++ Assets/Editor/MyPostprocessor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MyPostprocessor : AssetPostprocessor
+{
+    void OnPostprocessAssetbundleNameChanged(string path,
+            string previous, string next)
+    {
+        Debug.Log("Asset Changed" + path + " old: " + previous + " new: " + next);
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}

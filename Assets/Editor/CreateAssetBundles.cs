f7460055ba5483bfeb03e499df640cdd3e2207b1
Emery Porter <emeryporter99@gmail.com>
2019-07-25 13:15:01 +0200

    Will now force build the assetbundle

--- Assets/Editor/CreateAssetBundles.cs
+++ Assets/Editor/CreateAssetBundles.cs
@@ -3,7 +3,7 @@ using System.IO;
 
 public class CreateAssetBundles
 {
-    [MenuItem("Assets/Build AssetBundles")]
+    [MenuItem("Assets/Force Build AssetBundles")]
     static void BuildAllAssetBundles()
     {
         string assetBundleDirectory = "Assets/AssetBundles";
@@ -11,6 +11,6 @@ public class CreateAssetBundles
         {
             Directory.CreateDirectory(assetBundleDirectory);
         }
-        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.ForceRebuildAssetBundle, BuildTarget.StandaloneWindows);
     }
 }

7a18292e84b2ebd01eaa350d37747ceab03b96d9
Emery Porter <emeryporter99@gmail.com>
2019-07-31 12:53:58 +0200

    Continued Timestate implementation

--- Assets/Scripts/TimeState/TimestateManager.cs
+++ Assets/Scripts/TimeState/TimestateManager.cs
@@ -15,22 +15,29 @@ public static class TimestateManager
     public static System.Func<UniTask> doTransition;
     public static async UniTask SwitchTo(Timestate timestate)
     {
+        AsyncOperation loadTransition = SceneManager.LoadSceneAsync("Transition"); //Begin loading the transition scene
+        loadTransition.allowSceneActivation = false;
         await GameUtils.FadeCameraAsync(false, 5, true);  //Fade the scene
         if (currentTimestate != null) //Invoke the finished event
         {
             await currentTimestate.onFinished.OnInvokeAsync(new CancellationToken());
         }
         currentTimestate = timestate; //set the current timestate
-        await SceneManager.LoadSceneAsync("Transition"); //load the transition scene
+
+        //Finishing up the transition scene
+        await UniTask.WaitUntil(() => Mathf.Approximately(loadTransition.progress, .9f));
+        loadTransition.allowSceneActivation = true;
+        await loadTransition;
+
+
         await GameUtils.RefreshGameData(); //refresh the GameData so that it can be referenced properly
-        await GameUtils.FadeCameraAsync(true, 5, true); //fade in the scene
-        UniTask<List<AsyncOperation>> loadTimestate = LoadAsync();
-        await doTransition();
+        UniTask<List<AsyncOperation>> loadTimestate = LoadAsync(); //start loading the scene
+        await GameUtils.FadeCameraAsync(true, 5, true); //fade in the transition
+        await doTransition(); //invoke transition
         await loadTimestate;
         List<AsyncOperation> asyncOperations = loadTimestate.Result;
         await GameUtils.FadeCameraAsync(false, 5, true); //fade out the transition scene
         await FinishLoadingAsync(asyncOperations);
-        await SceneManager.UnloadSceneAsync("Transition"); //unload the transition scene
         await GameUtils.RefreshGameData(); //refresh the game data so that it reflects the new scene 
         await GameUtils.FadeCameraAsync(true, 5, true); //fade in the scene
         timestate.onStarted.Invoke(); //invoke the started event
@@ -41,29 +48,28 @@ public static class TimestateManager
         foreach (AsyncOperation asyncOperation in asyncOperations)
         {
             asyncOperation.allowSceneActivation = true;
+            await asyncOperation;
         }
-        await UniTask.Yield();
     }
 
     private static async UniTask<List<AsyncOperation>> LoadAsync()
     {
         List<AsyncOperation> asyncOperations = new List<AsyncOperation>();
-        foreach (SceneSetup scene in currentTimestate.timestateScriptableObject.sceneSetups)
+        List<string> scenePaths = currentTimestate.timestateScriptableObject.scenePaths;
+        for (int i = 0; i < scenePaths.Count; i++)
         {
-            if (scene.IsLoaded)
+            AsyncOperation sceneLoadTask;
+            if (i == 0)
             {
-                AsyncOperation sceneLoadTask = SceneManager.LoadSceneAsync(scene.Path, LoadSceneMode.Additive);
-                sceneLoadTask.allowSceneActivation = false;
-                while (!Mathf.Approximately(sceneLoadTask.progress, .9f))
-                {
-                    await UniTask.Yield();
-                }
-                asyncOperations.Add(sceneLoadTask);
-                //if (scene.IsActive)
-                //{
-                //    SceneManager.SetActiveScene(SceneManager.GetSceneAt(SceneManager.sceneCount - 1));
-                //}
+                sceneLoadTask = SceneManager.LoadSceneAsync(scenePaths[0], LoadSceneMode.Single);
             }
+            else
+            {
+                sceneLoadTask = SceneManager.LoadSceneAsync(scenePaths[i], LoadSceneMode.Additive);
+            }
+            sceneLoadTask.allowSceneActivation = false;
+            await UniTask.WaitUntil(() => Mathf.Approximately(sceneLoadTask.progress, .9f));
+            asyncOperations.Add(sceneLoadTask);
         }
         return asyncOperations;
     }

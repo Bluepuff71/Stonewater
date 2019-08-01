ab4b423b6d9fe3e8f1a5b468d59e169675e63895
Emery Porter <emeryporter99@gmail.com>
2019-08-01 13:39:05 +0200

    Cleaned up the project

---
+++ Assets/Scripts/Bluepuff/Editor/MyPostprocessor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+namespace Bluepuff.Utils
+{
+    public class Postprocessor : AssetPostprocessor
+    {
+        void OnPostprocessAssetbundleNameChanged(string path,
+                string previous, string next)
+        {
+            Debug.Log("Asset Changed: " + path + " old: " + previous + " new: " + next);
+            BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        }
+    }
+}

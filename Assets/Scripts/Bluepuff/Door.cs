4021965444bd5854508e70814f8ce02ee7708410
Emery Porter <emeryporter99@gmail.com>
2019-08-30 16:57:53 -0500

    Initial Implementation
    Currently broken, had to leave work.

---
+++ Assets/Scripts/Bluepuff/Door.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Bluepuff
+{
+    public enum DoorBehaviour
+    {
+        SWITCH_CAMERAS,
+        TELEPORT,
+        CUSTOM
+    }
+    [ExecuteAlways]
+    public class Door : MonoBehaviour
+    {
+        //Camera switcher editor will handle selecting the cameras
+        public GameObject[] cameraObjs = new GameObject[2];
+
+        public DoorBehaviour behaviour;
+
+        private Dictionary<PlayerController, Vector3> entryPosDict = new Dictionary<PlayerController, Vector3>();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (Application.isPlaying)
+            {
+                gameObject.layer = 9;
+                //This "bakes" all of the cameras
+                Array.ForEach(cameraObjs, (cameraObj) =>
+                {
+                    if (!cameraObj.tag.Equals("MainCamera"))
+                    {
+                        GameObject bakedCamera = Instantiate(new GameObject("--Baked Camera--"), cameraObj.transform);
+                        bakedCamera.transform.position = cameraObj.transform.position;
+                        bakedCamera.transform.rotation = cameraObj.transform.rotation;
+                        bakedCamera.transform.localScale = cameraObj.transform.localScale;
+                        Destroy(cameraObj);
+                    }
+                });
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (Application.isPlaying)
+            {
+                Debug.Log("Endter");
+                PlayerController playerController = other.GetComponentInParent<PlayerController>();
+                if (playerController)
+                {
+                    entryPosDict.Add(playerController, playerController.transform.position);
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (Application.isPlaying)
+            {
+                PlayerController playerController = other.GetComponentInParent<PlayerController>();
+                if (playerController)
+                {
+                    Vector3 exitPos = playerController.transform.position;
+                    if (entryPosDict.TryGetValue(playerController, out Vector3 entryPos))
+                    {
+                        // Bit shift the index of the layer (9) to get a bit mask
+                        int layerMask = 1 << 9;
+
+                        RaycastHit hit;
+                        // Does the ray intersect any objects excluding the player layer
+                        if (Physics.Raycast(entryPos, entryPos - exitPos, out hit, Mathf.Infinity, layerMask))
+                        {
+                            if (hit.transform == this.transform)
+                            {
+                                Debug.Log("Exited Door");
+                            }
+                            else
+                            {
+                                Debug.LogErrorFormat("Door: {0} and Door: {1} are placed too close to eachother.", this.name, hit.transform.name);
+                            }
+                        }
+                        else
+                        {
+                            Debug.Log("Turned Around");
+                        }
+                        Debug.DrawLine(entryPos, exitPos, Color.white, 20);
+                        entryPosDict.Remove(playerController);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Bluepuff/Editor/DoorEditor.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+#if UNITY_EDITOR
+namespace Bluepuff
+{
+    [CustomEditor(typeof(Door))]
+    public class DoorEditor : Editor
+    {
+        private Door door;
+        private void Awake()
+        {
+            door = (Door)target;
+        }
+        public void OnSceneGUI()
+        {
+            if (door.cameraObjs[0])
+            {
+                Handles.DrawLine(door.transform.position, door.cameraObjs[0].transform.position);
+            }
+            if (door.cameraObjs[1])
+            {
+                Handles.DrawLine(door.transform.position, door.cameraObjs[1].transform.position);
+            }
+        }
+        public override void OnInspectorGUI()
+        {
+            door.behaviour = (DoorBehaviour)EditorGUILayout.EnumPopup("Door Behaviour", door.behaviour);
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            switch (door.behaviour)
+            {
+                case DoorBehaviour.SWITCH_CAMERAS:
+                    {
+                        EditorGUILayout.BeginHorizontal();
+                        GUILayout.FlexibleSpace();
+                        EditorGUILayout.LabelField("Cameras to switch between.");
+                        GUILayout.FlexibleSpace();
+                        EditorGUILayout.EndHorizontal();
+                        EditorGUI.BeginChangeCheck();
+                        Camera[] temp = new Camera[2];
+                        temp[0] = EditorGUILayout.ObjectField((door.cameraObjs[0]) ? door.cameraObjs[0].GetComponent<Camera>() : null, typeof(Camera), true) as Camera;
+                        temp[1] = EditorGUILayout.ObjectField((door.cameraObjs[1]) ? door.cameraObjs[1].GetComponent<Camera>() : null, typeof(Camera), true) as Camera;
+                        if(temp[0] != null && temp[1] != null && temp[0] == temp[1])
+                        {
+                            Debug.LogError("The selected camera has already been selected. Changes reverted.");
+                        }
+                        else if(temp[0] != temp[1])
+                        {
+                            try
+                            {
+                                door.cameraObjs[0] = temp[0].gameObject;
+                            }
+                            catch (System.NullReferenceException)
+                            {
+                                door.cameraObjs[0] = null;
+                            }
+                            try
+                            {
+                                door.cameraObjs[1] = temp[1].gameObject;
+                            }
+                            catch (System.NullReferenceException)
+                            {
+                                door.cameraObjs[1] = null;
+                            }
+                        }
+                        
+                        if (EditorGUI.EndChangeCheck() && !Application.isPlaying)
+                        {
+                            EditorUtility.SetDirty(door);
+                            EditorSceneManager.MarkSceneDirty(door.gameObject.scene);
+                        }
+                        break;
+                    }
+                default:
+                    break;
+            }
+
+        }
+    }
+}
+#endif
